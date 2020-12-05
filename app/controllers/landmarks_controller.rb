f5443965d2cb445a4538c6397039bd773cc5d97d
joshsrai <joshsrai@gmail.com>
2020-12-05 10:53:10 -0500

    commit tree 849ea131b43a47dbc8c25cad6f7c454b2244d348

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,36 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get "/landmarks" do
+    @landmarks = Landmark.all #this will load up all of the landmarks from the database
+    erb :"landmarks/index"
+  end
+
+  get "/landmarks/new" do
+    erb :"landmarks/new" 
+  end
+
+  post "/landmarks" do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end 
+
+  get '/landmarks/:id/edit' do  #load edit form
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+  
+  patch '/landmarks/:id' do #edit action
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+    
+    redirect to "/landmarks/#{@landmark.id}"
+end 
+
+
+
 end
